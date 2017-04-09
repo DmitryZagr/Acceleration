382fd65c36f2f3dd34cbb96d6f8b70613de32789
TaxaRun <taxarun@gmail.com>
2017-04-09 22:34:27 +0300

    Init commit

---
+++ app/src/main/java/com/devteam/acceleration/ChatActivity.java
@@ -0,0 +1,21 @@
+package com.devteam.acceleration;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.devteam.acceleration.dummy.DummyContent;
+
+public class ChatActivity extends AppCompatActivity implements AnswersFragment.OnListFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
+}
