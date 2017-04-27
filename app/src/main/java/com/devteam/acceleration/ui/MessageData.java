d866d87bcc41e73e7a77c9feb926514ad5dc04f0
TaxaRun <taxarun@gmail.com>
2017-04-27 19:53:20 +0300

    Improvements in message design

--- app/src/main/java/com/devteam/acceleration/ui/MessageData.java
+++ app/src/main/java/com/devteam/acceleration/ui/MessageData.java
@@ -15,19 +15,19 @@ public class MessageData {
     public static final List<MessageModel> items = new ArrayList<MessageModel>();
     public static AtomicInteger count = new AtomicInteger(0);
 
-//    private static final String[] messages = new String[]{
-//            "Hello!", "How are you?", "Now we are going to talk about...Lorem Ipsum. Lorem Ipsum" +
-//            " - это текст-\"рыба\", \" \n" +
-//            "\"часто используемый в печати и вэб-дизайне. Lorem Ipsum является \" \n" +
-//            "\"стандартной \"рыбой\" для текстов на латинице с начала XVI века.",
-//            "Flushing caches. Disabling v-sync. SetSwapInterval() interval: 0 not set."
-//    };
+    private static final String[] messages = new String[]{
+            "Hello! Send something to me!", "How are you?", "Now we are going to talk about...Lorem Ipsum. Lorem Ipsum" +
+            " - это текст-\"рыба\", \" \n" +
+            "\"часто используемый в печати и вэб-дизайне. Lorem Ipsum является \" \n" +
+            "\"стандартной \"рыбой\" для текстов на латинице с начала XVI века.",
+            "Flushing caches. Disabling v-sync. SetSwapInterval() interval: 0 not set."
+    };
 
     static {
-        // Add some sample items.
-//        for (int i = 1; i <= 3; i++) {
-            addItem(createMessage(1));
-//        }
+//         Add some sample items.
+        for (int i = 1; i <= 25; i++) {
+            addItem(createMessage(i));
+        }
     }
 
     public static void addItem(MessageModel item) {
@@ -39,8 +39,8 @@ public class MessageData {
     private static MessageModel createMessage(int position) {
         Random generator = new Random();
         return new MessageModel(String.valueOf(position),
-                "Hello! Send something to me!",
-                INCOMING_MESSAGE);
+                messages[generator.nextInt(messages.length)],
+                position % 2);
     }
 
     public static class MessageModel {
